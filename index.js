82d2b5a957067369493b2a63f4616618db60f89a
André Vargas Roo <a.vargas252@icloud.com>
2023-01-19 21:44:31 -0500

    commit tree 18b8045397e1314747a210347542647203f6361e

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+
+//for (let age = 30; age < 40; age++) {
+ //   console.log(`I'm ${age} years old. Happy birthday to me!`);
+//}
+
+//const gifts = ["teddy bear", "drone", "doll"];
+
+//    for (let i = 0; i < gifts.length; i++) {
+    //    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+   // }
+
+   // return gifts;
+//}
+
+//wrapGifts(gifts);
+
+const names = ["Brigette", "Reed", "Marcus", "Alice"];
+const event = "Christmas"
+
+function writeCards(names, event) {
+    const card = [];
+    for (let n = 0; n < names.length; n++) {
+        card.push(`Thank you, ${names[n]}, for the wonderful ${event} gift!`)
+    }
+    return card;
+}
+
+function countDown() {
+    let count = 10;
+    while (count > -1) {
+        console.log(count--);
+    }
+}
